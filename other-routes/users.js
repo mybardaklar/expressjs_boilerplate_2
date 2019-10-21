00ea248e567b6fffdb0ba2219aec08fa7f84717f
Muhammed Yaşar Bardaklar <myasarbardaklar47@gmail.com>
2019-10-21 13:29:17 +0300

    remove other-routes folder

--- other-routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.status(200).json({ title: 'routes/users.js' })
-})
-
-module.exports = router
